936df8cbef72139fdf87061515fca8ae3e029fc3
Iter <correu.comercial@mailo.com>
2023-12-14 20:44:23 +0100

    Fixing stupidity

--- esp32/PlatformIO/TTGO-Wifi/src/main.cpp
+++ esp32/PlatformIO/TTGO-Wifi/src/main.cpp
@@ -2,31 +2,32 @@
 #include <WiFi.h>
 
 // constants
-const char *ssid = "<wifi_name>";
-const char *password = "<wifi_pwd>";
-
+const char *ssid = "MIWIFI_2G_KqRx";
+const char *password = "VIENA2017";
 // put function declarations here:
-// int myFunction(int, int);
-void initWifi()
+void initWiFi()
 {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.print("Connectant a la wifi...");
+  Serial.print("Connecting to WiFi ..");
   while (WiFi.status() != WL_CONNECTED)
   {
-    Serial.print(".");
+    Serial.print('.');
     delay(1000);
   }
   Serial.println(WiFi.localIP());
 }
-
+// int myFunction(int, int);
 void setup()
 {
   // put your setup code here, to run once:
   // int result = myFunction(2, 3);
-  // Serial.begin(9600);
-  Serial.begin(115200);
-  initWifi();
+  Serial.begin(9600);
+  // Serial.begin(115200);
+
+  initWiFi();
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  // Serial.begin(115200);
 }
 
 void loop()
